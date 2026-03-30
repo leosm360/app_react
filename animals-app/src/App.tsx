37c03783ba0e17c71a31b999fa3aa5205476b580
Francisco Leo <leo@segmind.com.br>
2026-03-30 14:55:28 -0300

    Alteracao de titulo para gerar conflito

--- animals-app/src/App.tsx
+++ animals-app/src/App.tsx
@@ -3,7 +3,7 @@ import { AnimalsPage } from "./pages/AnimalsPage"
 function App() {
   return (
     <div>
-      <h1>Mini Animals App - Branch Feature</h1>
+      <h1>Mini Animals App - Main Alterada</h1>
       <p>Gestão simples de animais</p>
       <AnimalsPage />
     </div>
